2e8445cc7bb650f93d843fb5ce82611569ac1d66
cphalder <91915610+cphalder@users.noreply.github.com>
2022-08-28 10:29:57 +0100

    Created classes to link with JS.

---
+++ app.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll(".data-number-js");
+const operatorButtons = document.querySelectorAll(".data-operator-js");
+const deleteButton = document.querySelector(".data-delete-js");
+const clearButton = document.querySelector(".data-clear-js");
+const equalButton = document.querySelector(".data-equal-js");
+const previousInputTextElement = document.querySelector(".previous-input-js");
+const currentInputTextElement = document.querySelector(".current-input-js");
